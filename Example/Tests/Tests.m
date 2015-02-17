55b47029db1ae2e110ae5b67878fe5d3fee0176c
Alexander Widerberg <alexander.widerberg@cloudme.com>
2015-02-17 11:46:45 +0100

    Moved test file to example project (no need to be in the resource-bundle for the lib)

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -15,18 +15,13 @@ describe(@"these should pass", ^{
     __block NSString* _filePath = nil;
 
     beforeAll(^{
+        // Setup test NSData
          _data = [NSData dataWithBytes:"AWFileHash" length:10];
-        // [data md5] == f8b38b91e8972b2cffcf46723c755089
-        // [data sha1] == e100884df5826e4e2c36f784521f7b4c74809082
-        // [data sha512] == A83343A46AABC469E12483EC6F4A4620516B50AD2A810226270E7EFA4851E8F69065A47B6C6EEEE8ED9006361F79665CFA0E481AE69EF873B55653220B0A578A
 
         // Fetch the test file path
         NSString *mainBundlePath = [[NSBundle bundleForClass:[self class]] resourcePath];
         NSBundle *mainBundle = [NSBundle bundleWithPath:mainBundlePath];
-        NSString *podBundlePath = [mainBundle pathForResource:@"AWFileHash" ofType:@"bundle"];
-        NSBundle *podBundle = [NSBundle bundleWithPath:podBundlePath];
-         _filePath = [podBundle pathForResource:@"flower" ofType:@"png"];
-
+         _filePath = [mainBundle pathForResource:@"flower" ofType:@"png"];
     });
 
     beforeEach(^{
@@ -47,15 +42,15 @@ describe(@"these should pass", ^{
 
     // File tests
     it(@"can calculate MD5 of nsdata", ^{
-        expect([AWFileHash md5HashOfFileAtPath:_filePath]).to.equal(@"f8b38b91e8972b2cffcf46723c755089");
+        expect([AWFileHash md5HashOfFileAtPath:_filePath]).to.equal(@"e2b73ef09ebf853047fe1dcb6ad0862a");
     });
 
     it(@"can calculate SHA1 of nsdata", ^{
-        expect([AWFileHash sha1HashOfFileAtPath:_filePath]).to.equal(@"e100884df5826e4e2c36f784521f7b4c74809082");
+        expect([AWFileHash sha1HashOfFileAtPath:_filePath]).to.equal(@"91f90cc1f585fb6ebe6980954a33e2da27257edf");
     });
 
     it(@"can calculate SHA512 of nsdata", ^{
-        expect([AWFileHash sha512HashOfFileAtPath:_filePath]).to.equal(@"a83343a46aabc469e12483ec6f4a4620516b50ad2a810226270e7efa4851e8f69065a47b6c6eeee8ed9006361f79665cfa0e481ae69ef873b55653220b0a578a");
+        expect([AWFileHash sha512HashOfFileAtPath:_filePath]).to.equal(@"a428ad0b1ef9b09b2c7477cce93b0ccd052c70423810778f042dc003c1941129c54d4e82d11b76dad2dddd881cd6fda0063a57793ea95aafcb54a57721391a57");
     });
     
     /*it(@"can read", ^{
@@ -73,7 +68,8 @@ describe(@"these should pass", ^{
     });
 
     afterAll(^{
-        data = nil;
+        _data = nil;
+        _filePath = nil;
     });
 });
 
